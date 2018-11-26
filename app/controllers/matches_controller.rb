114f197da82c34a100dc03f9b3de9f214582952f
Alessandra Huydts <A.huydts@yahoo.de>
2018-11-26 16:22:45 +0200

    working on the matching controller

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,7 @@
+class MatchesController < ApplicationController
+
+  def create
+
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  acts_as_votable
+  acts_as_voter
+
+
+
+def self.matched?(user2)
+  self.liked_by user2
 end
+
+# controller:
+  def matching
+    @user = User.find_params[:id]
+    current_user.likes @user
+    if current_user.matched?(@user)
+      match = Match.new(user1_id: current_user, user2_id: @user)
+      match.save
+      redirect_to root
+    end
+  end
+end
+
+
+
