cc3a063acaace60a03d01dfbc92c04454d67f716
Salo Charabati <salochara@gmail.com>
2018-11-27 14:16:51 +0200

    setting tags for profile done

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
-  resources :users, only: [:edit, :update, :show] do 
+
+  resources :users, only: [:edit, :update, :show] do
      member do
         post "/matching", to: "users#matching"
       end
-  end 
+  end
 
   # routes for user registration
   get 'registration/cuisines', to: 'registration#cuisines_show'
   post 'registration/cuisines/edit', to: 'registration#cuisines_edit'
   get 'registration/interests', to: 'registration#interests_show'
   post 'registration/interests/edit', to: 'registration#interests_edit'
+  get 'registration/bio', to: 'registration#bio_show'
+  post 'registration/bio/edit', to: 'registration#bio_edit'
 end
