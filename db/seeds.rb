c13d1459dc30cb31f051f2e91ea808facaf372d3
Alessandra Huydts <A.huydts@yahoo.de>
2018-11-29 23:31:08 +0200

    added better quality images to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ user_attributes = [
     password: "12345678",
     latitude: 32.0763317,
     longitude: 34.7661526,
-    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543486054/14947430_1430518190304704_2215749094286475860_n.jpg",
+    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543526849/ale.jpg",
     bio: " I am half German and half Dutch, but currently live in Tel Aviv. I love to cook, bake, swim and spend time with friends. I am very exited to try this application:)"
   },
   {
@@ -22,7 +22,7 @@ user_attributes = [
     password: "12345678",
     latitude: 32.0763337,
     longitude: 34.7660526,
-    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543486054/22789141_10100120106986471_2166716107713851198_n.jpg",
+    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543526868/victoria.jpg",
     bio: "Hi I am Victoria, I currently live in Tel Aviv and I would love to cook with somone. I am especially interested in learning how to make Israeli dishes. I have lived all over the world so far, and I am looking forward to a nice homecooked dinner together:)"
   },
   {
@@ -32,7 +32,7 @@ user_attributes = [
     password: "12345678",
     latitude: 32.0763317,
     longitude: 34.7660626,
-    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543486054/37643942_10156841776529050_3929005442084110336_n.jpg",
+    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543526868/salo.jpg",
     bio: "I would like to work in the high-tech scene in the future. I'm a 3rd year student of CS in Mexico City. I'm a technology enthusiast and looking forward to get the most out of the bootcamp! Very excited to start this journey :)"
   },
   {
@@ -42,7 +42,7 @@ user_attributes = [
     password: "12345678",
     latitude: 48.7228535,
     longitude: 9.21835617,
-    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543486054/T02NE0241-UDA0HQJ5V-243dbed7e43f-72.jpg",
+    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543526849/emmanuel.jpg",
     bio: "I had my Diploma in Business Administration in Cambridge International College UK. Am now upcoming fully stack web Developer leading to become a programmer."
   },
   {
@@ -52,7 +52,7 @@ user_attributes = [
     password: "12345678",
     latitude: 48.7228535,
     longitude: 9.21835617,
-    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543486054/46458137_2061704790559867_4097574108064120832_n.jpg",
+    facebook_picture_url: "https://res.cloudinary.com/dmeg8bow6/image/upload/v1543526849/baraa.jpg",
     bio: "I worked in marketing before, but I always had a passion for startups, technology and making a change, after failing in my first startup I saw that coding skills is what's missing. Le Wagon and to the next startup!"
   },
 ]
@@ -62,7 +62,7 @@ puts "Created #{User.count} users"
 puts "Assigning Tags to Alessandra...."
 alessandra = User.find_by(first_name: "Alessandra")
 alessandra.cuisine_list.add("Israeli", "Japanese", "Italian")
-alessandra.interest_list.add("swim", "cook", "bake", "code", "travel")
+alessandra.interest_list.add("swim", "cook", "code", "travel")
 alessandra.restriction_list.add("Gluten-Free", "Lactose-Free")
 alessandra.save
 puts "Alessandra has the following tags #{alessandra.cuisine_list}, #{alessandra.interest_list}, #{alessandra.restriction_list}"
@@ -107,5 +107,3 @@ Match.create!(user1: User.first, user2: User.third)
 Match.create!(user1: User.fourth, user2: User.last)
 
 puts "Created #{Match.count} matches"
-
-
