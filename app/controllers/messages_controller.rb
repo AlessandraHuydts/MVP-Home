e0cda093f44e2a6d6c5892e86d24a963a5b7a1be
Salo Charabati <salochara@gmail.com>
2018-11-30 13:03:45 +0200

    chat is working, missing config for production and to create a chatroom once a match is created

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,8 +5,9 @@ class MessagesController < ApplicationController
     @message.chat_room = @chat_room
     @message.user = current_user
     if @message.save
-      ActionCable.server.broadcast("chat_room_#{@chat_room.id}" , {
-                                     message_partial: render(partial: "messages/message", locals: { message: @message})
+      ActionCable.server.broadcast("chat_room_#{@chat_room.id}" ,
+                                   {
+                                     message_partial: render(partial: "messages/message", locals: { message: @message, user_is_messages_author: false })
       })
       respond_to do |format|
         format.html { redirect_to chat_room_path(@chat_room) }
