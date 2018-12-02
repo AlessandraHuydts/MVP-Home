8e4c10bdb76743a7398e8e5283a581c15ede03a3
Alessandra Huydts <A.huydts@yahoo.de>
2018-12-02 12:49:49 +0200

    now the right seeds

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -8,8 +8,15 @@ class ChatRoomsController < ApplicationController
       if chat.match.user1 == current_user || chat.match.user2 == current_user
         @my_chats = []
         @my_chats << chat
-
       end
+    end
+  end
+
+  def match_name(chat)
+    if chat.match.user1 == current_user
+      chat.match.user2.first_name
+    else
+      chat.match.user1.first_name
     end
   end
 end
