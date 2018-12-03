36a1499b6415fc68eb7fc0359e82ff05cfac9421
Salo Charabati <salochara@gmail.com>
2018-12-03 12:48:25 +0200

    navbar fixed with corresponding icons in diffreent views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,15 @@ import "bootstrap";
 import { geoFindMe } from 'packs/location';
 import { scrollLastMessageIntoView } from 'packs/scroll';
 import { fadeOutAlert } from 'packs/fadeOutAlert';
+import { hideNavbarIcon } from 'packs/hideMatchingIconInNavbar';
+
 geoFindMe();
 // scrollLastMessageIntoView();
 if(document.getElementById('alert')){
   fadeOutAlert();
+}
+
+if (document.getElementById('target-hide-icon-navbar')){
+  hideNavbarIcon();
 }
 
