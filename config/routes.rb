15d812dd11bc4dfc03158bf5e40ef595c69d742a
Salo Charabati <salochara@gmail.com>
2018-12-03 10:59:08 +0200

    changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
+
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 
   resources :users, only: [:edit, :update, :show] do
     member do
