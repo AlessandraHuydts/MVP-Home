c67d35c43a56e32287b9bdb911988ffa6c605536
Salo Charabati <salochara@gmail.com>
2018-12-05 14:08:43 +0200

    no location for demo nicght

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,15 @@
 import "bootstrap";
 import { geoFindMe } from 'packs/location';
-import { scrollLastMessageIntoView } from 'packs/scroll';
+//import { scrollLastMessageIntoView } from 'packs/scroll';
 import { fadeOutAlert } from 'packs/fadeOutAlert';
 import { hideNavbarIcon } from 'packs/hideMatchingIconInNavbar';
 import { add_recipe_toggle } from '../components/recipe_toggle';
 
+// scrollLastMessageIntoView();
 if(document.querySelector('.search_query')){
   geoFindMe();
 }
 
-// scrollLastMessageIntoView();
 if(document.getElementById('alert')){
   fadeOutAlert();
 }
@@ -18,5 +18,8 @@ if (document.getElementById('target-hide-icon-navbar')){
   hideNavbarIcon();
 }
 
-add_recipe_toggle();
+if (document.getElementById('target-for-recipe')){
+  add_recipe_toggle();
+}
+
 
